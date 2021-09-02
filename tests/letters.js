bec9be81cad876f4dde8fbc5248a3e2bcd3f805e
Vladimir Lunkin <vova.gold.201427@gmail.com>
2021-09-02 17:34:31 +0300

    добавил пробелы

--- tests/letters.js
+++ tests/letters.js
@@ -1,94 +1,94 @@
 'use strict';
 
 QUnit.module('Тестируем функцию letters', function () {
-	QUnit.test('Оставляет без изменений строки, где все символы уникальны', function (assert) {
-		assert.strictEqual(letters('1234'), '1234');
-		assert.strictEqual(letters('abcd'), 'abcd');
-		assert.strictEqual(letters('олдж фыва'), 'олдж фыва');
-		assert.strictEqual(letters(',.;=\n\t '), ',.;=\n\t ');
+    QUnit.test('Оставляет без изменений строки, где все символы уникальны', function (assert) {
+        assert.strictEqual(letters('1234'), '1234');
+        assert.strictEqual(letters('abcd'), 'abcd');
+        assert.strictEqual(letters('олдж фыва'), 'олдж фыва');
+        assert.strictEqual(letters(',.;=\n\t '), ',.;=\n\t ');
 
-		assert.strictEqual(letters('1234', true), '1234');
-		assert.strictEqual(letters('abcd', true), 'abcd');
-		assert.strictEqual(letters('олдж фыва', true), 'олдж фыва');
-		assert.strictEqual(letters(',.;=\n\t ', true), ',.;=\n\t ');
+        assert.strictEqual(letters('1234', true), '1234');
+        assert.strictEqual(letters('abcd', true), 'abcd');
+        assert.strictEqual(letters('олдж фыва', true), 'олдж фыва');
+        assert.strictEqual(letters(',.;=\n\t ', true), ',.;=\n\t ');
 
-		assert.strictEqual(letters('1234', false), '1234');
-		assert.strictEqual(letters('abcd', false), 'abcd');
-		assert.strictEqual(letters('олдж фыва', false), 'олдж фыва');
-		assert.strictEqual(letters(',.;=\n\t ', false), ',.;=\n\t ');
-	});
+        assert.strictEqual(letters('1234', false), '1234');
+        assert.strictEqual(letters('abcd', false), 'abcd');
+        assert.strictEqual(letters('олдж фыва', false), 'олдж фыва');
+        assert.strictEqual(letters(',.;=\n\t ', false), ',.;=\n\t ');
+    });
 
-	QUnit.test('Удаляет идущие подряд буквы', function (assert) {
-		assert.strictEqual(letters('111'), '');
-		assert.strictEqual(letters('www'), '');
-		assert.strictEqual(letters('...'), '');
-		assert.strictEqual(letters('   '), '');
-	});
+    QUnit.test('Удаляет идущие подряд буквы', function (assert) {
+        assert.strictEqual(letters('111'), '');
+        assert.strictEqual(letters('www'), '');
+        assert.strictEqual(letters('...'), '');
+        assert.strictEqual(letters('   '), '');
+    });
 
-	QUnit.test('Оставляет первую букву, остальные удаляет', function (assert) {
-		assert.strictEqual(letters('121', true), '12');
-		assert.strictEqual(letters('wWw', true), 'wW');
-		assert.strictEqual(letters('.-.', true), '.-');
-		assert.strictEqual(letters(' 0 ', true), ' 0');
-	});
+    QUnit.test('Оставляет первую букву, остальные удаляет', function (assert) {
+        assert.strictEqual(letters('121', true), '12');
+        assert.strictEqual(letters('wWw', true), 'wW');
+        assert.strictEqual(letters('.-.', true), '.-');
+        assert.strictEqual(letters(' 0 ', true), ' 0');
+    });
 
-	QUnit.test('Оставляет последнюю букву, остальные удаляет', function (assert) {
-		assert.strictEqual(letters('121', false), '21');
-		assert.strictEqual(letters('wWw', false), 'Ww');
-		assert.strictEqual(letters('.-.', false), '-.');
-		assert.strictEqual(letters(' 0 ', false), '0 ');
-	});
+    QUnit.test('Оставляет последнюю букву, остальные удаляет', function (assert) {
+        assert.strictEqual(letters('121', false), '21');
+        assert.strictEqual(letters('wWw', false), 'Ww');
+        assert.strictEqual(letters('.-.', false), '-.');
+        assert.strictEqual(letters(' 0 ', false), '0 ');
+    });
 
-	QUnit.test('Удаляет повторяющиеся буквы в разных словах', function (assert) {
-		assert.strictEqual(letters('привет, мир'), 'пвет, м');
-		assert.strictEqual(letters('hello, world'), 'he, wrd');
-		assert.strictEqual(letters('мама мыла раму'), 'ылру');
-		assert.strictEqual(letters('"Кукареку!", сказал Петух'), 'Кр!,сзлПтх');
+    QUnit.test('Удаляет повторяющиеся буквы в разных словах', function (assert) {
+        assert.strictEqual(letters('привет, мир'), 'пвет, м');
+        assert.strictEqual(letters('hello, world'), 'he, wrd');
+        assert.strictEqual(letters('мама мыла раму'), 'ылру');
+        assert.strictEqual(letters('"Кукареку!", сказал Петух'), 'Кр!,сзлПтх');
 
-		assert.strictEqual(letters('мама мыла раму', true), 'ма ылру');
-		assert.strictEqual(letters('от топота копыт', true), 'от пакы');
-		assert.strictEqual(letters('hello world', true), 'helo wrd');
+        assert.strictEqual(letters('мама мыла раму', true), 'ма ылру');
+        assert.strictEqual(letters('от топота копыт', true), 'от пакы');
+        assert.strictEqual(letters('hello world', true), 'helo wrd');
 
-		assert.strictEqual(letters('мама мыла раму', false), 'ыл раму');
-		assert.strictEqual(letters('от топота копыт', false), 'а копыт');
-		assert.strictEqual(letters('hello world', false), 'he world');
-	});
+        assert.strictEqual(letters('мама мыла раму', false), 'ыл раму');
+        assert.strictEqual(letters('от топота копыт', false), 'а копыт');
+        assert.strictEqual(letters('hello world', false), 'he world');
+    });
 
-	QUnit.test('Дополнительные тесты', function (assert) {
-		assert.strictEqual(letters('фффффффффффп'), 'п');
-		assert.strictEqual(letters(''), '');
-		assert.strictEqual(letters('       мммм      '), '');
-		assert.strictEqual(letters('122222222222222223'), '13');
+    QUnit.test('Дополнительные тесты', function (assert) {
+        assert.strictEqual(letters('фффффффффффп'), 'п');
+        assert.strictEqual(letters(''), '');
+        assert.strictEqual(letters('       мммм      '), '');
+        assert.strictEqual(letters('122222222222222223'), '13');
 
-		assert.strictEqual(letters('фффффффффффп', true), 'фп');
-		assert.strictEqual(letters('', true), '');
-		assert.strictEqual(letters('       мммм      ', true), ' м');
-		assert.strictEqual(letters('12222222221322222223', true), '123');
+        assert.strictEqual(letters('фффффффффффп', true), 'фп');
+        assert.strictEqual(letters('', true), '');
+        assert.strictEqual(letters('       мммм      ', true), ' м');
+        assert.strictEqual(letters('12222222221322222223', true), '123');
 
-		assert.strictEqual(letters('фффффффффффп', false), 'фп');
-		assert.strictEqual(letters('', false), '');
-		assert.strictEqual(letters('       мммм      ', false), 'м ');
-		assert.strictEqual(letters('12222222221322222223', false), '123');
-	});
+        assert.strictEqual(letters('фффффффффффп', false), 'фп');
+        assert.strictEqual(letters('', false), '');
+        assert.strictEqual(letters('       мммм      ', false), 'м ');
+        assert.strictEqual(letters('12222222221322222223', false), '123');
+    });
 
-	QUnit.test('Неправильные типы аргументов', function (assert) {
-		assert.throws(
-			function() {
-				letters(null)
-			},
-			/the first argument is of type string or number/,
-			'null');
-		assert.throws(
-			function() {
-				letters('123', null)
-			},
-			/the second argument is optional boolean/,
-			'string, null');
-		assert.throws(
-			function() {
-				letters()
-			},
-			/the first argument is of type string or number/,
-			'no arguments');
-		});
+    QUnit.test('Неправильные типы аргументов', function (assert) {
+        assert.throws(
+            function () {
+                letters(null)
+            },
+            /the first argument is of type string or number/,
+            'null');
+        assert.throws(
+            function () {
+                letters('123', null)
+            },
+            /the second argument is optional boolean/,
+            'string, null');
+        assert.throws(
+            function () {
+                letters()
+            },
+            /the first argument is of type string or number/,
+            'no arguments');
+    });
 });
